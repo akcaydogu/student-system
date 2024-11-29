753732c3c78d51f2a262d698dafaba78f3047cc4
akcaydogu <akcay.dogukan61@gmail.com>
2024-11-29 16:15:36 +0300

    Giriş kısmı geliştirmeleri

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,119 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class Main {
 
     public static HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
     final static String adminName = "admin";
     final static String adminPass = "admin";
+    public static Scanner scanner = new Scanner(System.in);
 
 
-    public static boolean signIn(String username, String password) {
-        for (ArrayList x : map.values()) {
-            if (x.get(0).equals(username) && x.get(1).equals(password)) {
-                return true;
-            }
+    public static void operation(int x) {
+        switch (x) {
+            case 6:
+                authorization(2);
         }
+    }
+    public static boolean signIn(String username, String password, int phase) {
+        if(phase == 1) {
+            for (ArrayList<String> x : map.values()) {
+                System.out.println(x);
+                if (x.get(0).equals(username) && x.get(1).equals(password)) {
+                    return true;
+                }
+            }
+        } else if(phase == 2) {
+            System.out.println("Başarıyla giriş yapıldı!");
+            System.out.printf("Hoşgeldin %s! %n", map.get(username).get(2));
+            System.out.println("Öğrencileri görüntülemek için 1");
+            System.out.println("Ders programını görüntülemek için 2");
+            System.out.println("Duyuru eklemek için 3");
+            System.out.println("Bilgilerinizi değiştirmek için 4");
+            System.out.println("Öğretmenleri görüntülemek için 5");
+            System.out.println("Kayıt oluşturmak için 6");
+            int x = scanner.nextInt();
+            scanner.nextLine();
+            operation(x);
+        }
+
         return false;
     }
-    public static boolean signUp(String username, String password) {
+    //Kayıt
 
+    //username-password-isim-numara-yaş-duyuru
+    public static void signUp(String username, String password, String name, String id, int age, boolean isDuyuru, int permissionLevel) {
         for (String x : map.keySet()) {
             if(x.equals(username)) {
                 System.out.println("Bu kullanıcı adı ile kaydolan başka bir üye zaten mevcut!");
-                return false;
             }
         }
-        map.put(username, new ArrayList<String>());
+
+        map.put(username, new ArrayList<>());
         map.get(username).add(username);
         map.get(username).add(password);
-
-        return true;
+        map.get(username).add(name);
+        map.get(username).add(id);
+        map.get(username).add(age+"");
+        map.get(username).add(isDuyuru+"");
+        map.get(username).add(permissionLevel+"");
     }
 
-    public static void main(String[] args) {
-
-        map.put(adminName, new ArrayList<String>());
-        map.get(adminName).add(adminName);
-        map.get(adminName).add(adminPass);
-
-        // new file object
-        Scanner scanner = new Scanner(System.in);
-        int x = 0;
-
-        while (true){
-            System.out.println("1 for SignIn 2 for SignUp");
-            try {
-                x = scanner.nextInt();
-            } catch (Exception e) {
-                System.out.println("Lütfen sadece sayı giriniz!");
-            }
-            scanner.nextLine();
+    public static void authorization(int x) {
+        try {
             if(x == 1) {
-                System.out.println("Giriş sistemi");
                 System.out.print("Lütfen kullanıcı adınızı giriniz: ");
                 String username = scanner.nextLine();
                 System.out.print("Lütfen şifrenizi giriniz: ");
                 String password = scanner.nextLine();
-                if (signIn(username, password)) {
-                    System.out.println("Başarıyla giriş yapıldı!");
-                    System.out.printf("Hoşgeldin %s! %n", username);
+                if (signIn(username, password, 1)) {
+                    signIn(username, password, 2);
                 } else {
                     System.out.println("Yanlış kullanıcı adı veya şifre girdiniz!");
                 }
             } else if (x == 2) {
                 System.out.println("Kayıt Sistemi");
-                System.out.print("Lütfen kullanıcı adınızı giriniz: ");
+                System.out.print("Lütfen kullanıcı adını giriniz: ");
                 String username = scanner.nextLine();
-                System.out.print("Lütfen şifrenizi giriniz: ");
+                System.out.print("Lütfen şifre giriniz: ");
                 String password = scanner.nextLine();
-                signUp(username, password);
+                System.out.print("Lütfen ad giriniz: ");
+                String name = scanner.nextLine();
+                String uniqueID = UUID.randomUUID().toString();
+                System.out.print("Lütfen yaş giriniz: ");
+                int age = scanner.nextInt();
+                scanner.nextLine();
+                System.out.print("Lütfen yetki seviyesini giriniz 1/2/3 : ");
+                int permissionLevel = scanner.nextInt();
+                scanner.nextLine();
+                signUp(username, password, name, uniqueID, age, false, permissionLevel);
+                System.out.println(map);
             } else {
                 System.out.println("Lütfen 1 veya 2 numaralı işlemi seçiniz!");
             }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+
+        map.put(adminName, new ArrayList<>());
+        map.get(adminName).add(adminName);
+        map.get(adminName).add(adminPass);
+        map.get(adminName).add("Yönetici");
+        map.get(adminName).add(UUID.randomUUID().toString());
+        map.get(adminName).add(0+"");
+        map.get(adminName).add(false+"");
+        map.get(adminName).add(3+"");
+
+        while (true) {
+            System.out.println("Hoşgeldiniz!");
+            authorization(1);
+        }
+
 
     }
 
